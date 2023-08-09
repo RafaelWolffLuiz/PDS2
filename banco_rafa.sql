86b8afed430246d698a6c23738cb8e8fa9316bb9
RafaelWolffLuiz <126568580+RafaelWolffLuiz@users.noreply.github.com>
2023-08-08 21:39:13 -0300

    Add files via upload

---
+++ banco_rafa.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE rafa_aprendendo;
+
+USE rafa_aprendendo;
+
+CREATE TABLE pessoa (
+      idadepessoa int,
+      primeironome varchar(255),
+      ultimonome varchar(255),
+      cpfpessoa int
+      );
